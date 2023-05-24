4baf52f26f2b344682ac74c033bb0eceddef49f5
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-05-24 02:44:46 +0000

    code working

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+// first we get the HTML for the button
+
+// program to generate fibonacci series up to n terms
+function calculate() 
+{
+  // take input from the user
+  const number = parseInt(document.getElementById('number').value)
+  let number1 = 0, number2 = 1, nextTerm
+
+  for (let counter = 1; counter <= number; counter++) 
+  {
+  
+    nextTerm = number1 + number2
+    number1 = number2
+    number2 = nextTerm
+    console.log(number1)
+    //document.getElementById(number1).innerHTML = "<p>" + number1 + "</p><br>" * -1
+    document.getElementById('answer').innerHTML = 'The answer is ' + number1
+  }
+}  
+
